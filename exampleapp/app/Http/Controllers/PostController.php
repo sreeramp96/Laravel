99e41a8320664b7521d19252f4d19b3a79c06207
Sreeram P <itssreeramp@gmail.com>
2023-10-11 00:09:12 +0530

    Updated code

---
+++ exampleapp/app/Http/Controllers/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Posts;
+
+class PostController extends Controller
+{
+    public function showCreateForm()
+    {
+        return view('create-post');
+    }
+
+    public function storeNewPost(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+        $incomingFields['user_id'] = auth()->id();
+
+        Posts::create($incomingFields);
+    }
+}

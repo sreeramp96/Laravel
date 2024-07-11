dcea9776bda91d09b04b60b5b1219305a1db3965
sreeram.p@maxwellgeosystems.com <8j2NmM8rtsTFy->
2024-07-11 17:00:55 +0530

    patch-1.11

--- task-list/resources/views/index.blade.php
+++ task-list/resources/views/index.blade.php
@@ -3,21 +3,24 @@
 @section('title', 'The list of tasks')
 
 @section('content')
+<nav class="mb-4">
+    <a href="{{route('tasks.create')}}" class="link">
+        Add Task</a>
+</nav>
+
+@forelse ($tasks as $task)
 <div>
-    <a href="{{route('tasks.create')}}">Add Task</a>
+    <a href="{{route('tasks.show',['task'=>$task->id])}}" @@class('p-4', 'font-bold' )>
+        {{$task->title}}
+    </a>
 </div>
+@empty
+<div>There are no Tasks!</div>
+@endforelse
 
-    @forelse ($tasks as $task)
-    <div>
-        <a href="{{route('tasks.show',['task'=>$task->id])}}">{{$task->title}}</a>
-    </div>
-    @empty
-    <div>There are no Tasks!</div>  
-    @endforelse
-
-    @if ($tasks->count())
-    <nav>
-        {{$tasks->links()}}
-    </nav>
-    @endif
+@if ($tasks->count())
+<nav class="mt-4">
+    {{$tasks->links()}}
+</nav>
+@endif
 @endsection

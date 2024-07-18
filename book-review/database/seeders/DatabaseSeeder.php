ec0a8abf776eca4f0d46665b8b4d02444a870e24
sreeramp96 <itssreeramp@gmail.com>
2024-07-18 23:06:53 +0530

    Patch-1.0

---
+++ book-review/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Book;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Review;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+            Review::factory()->count($numReviews)
+                ->good()
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+            Review::factory()->count($numReviews)
+                ->average()
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+            Review::factory()->count($numReviews)
+                ->bad()
+                ->for($book)
+                ->create();
+        });
+    }
+}

6792c2f47d87836e7e798c8c1b404d3ec2fa0bc0
Sreeram P <itssreeramp@gmail.com>
2024-07-30 22:39:30 +0530

    patch-3.0

--- book-review/app/Http/Controllers/BookController.php
+++ book-review/app/Http/Controllers/BookController.php
@@ -13,7 +13,21 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $title = $request->input('title');
-        $books = Book::when($title, fn($query, $title) => $query->title($title))->get();
+        $filter = $request->input('filter', '');
+
+        $books = Book::when($title, fn($query, $title) => $query->title($title));
+
+        $books = match ($filter) {
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6months' => $books->popularLast6Months(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_6months' => $books->highestRatedLast6Months(),
+            default => $books->latest()->withAvgRating()->withReviewsCount()
+        };
+
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        // $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
+        $books = $books->get();
         return view('books.index', ['books' => $books]);
     }
 
@@ -36,9 +50,11 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(int $id)
     {
-        //
+        $cacheKey = 'book:' . $id;
+        $book = cache()->remember($cacheKey, 3600, fn() => Book::with(['reviews' => fn($query) => $query->Latest()])->withAvgRating()->withReviewsCount()->findOrFail($id));
+        return view('books.show', ['book' => $book]);
     }
 
     /**
--- book-review/resources/views/books/index.blade.php
+++ book-review/resources/views/books/index.blade.php
@@ -3,11 +3,33 @@
 @section('content')
 <h1 class="mb-10 text-2xl">Books</h1>
 
-<form action="{{route('books.index')}}" class="mb-4 flex items-center space-x-2">
-    <input type="text" name="title" placeholder="Search by title" id="" value="{{request('title')}}" class="input h-10">
+<form method="GET" action="{{route('books.index')}}" class="mb-4 flex items-center space-x-2">
+    <input type="text" name="title" placeholder="Search by title" id="" value="{{request('title')}}"
+        class="input h-10" />
+    <input type="hidden" name="filter" value="{{ request('filter') }}" />
     <button type="submit" class="btn h-10">Search</button>
     <a href="{{route('books.index')}}" class="btn h-10">Clear</a>
 </form>
+
+<div class="filter-container mb-4 flex">
+    @php
+    $filters = [
+    '' => 'Latest',
+    'popular_last_month' => 'Popular Last Month',
+    'popular_last_6months' => 'Popular Last 6 Months',
+    'highest_rated_last_month' => 'Highest Rated Last Month',
+    'highest_rated_last_6months' => 'Highest Rated Last 6 Months',
+    ];
+    @endphp
+
+    @foreach ($filters as $key => $label )
+    <a href="{{route('books.index',[...request()->query(),'filter' => $key])}}"
+        class="{{request('filter') === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item'}}">
+        {{$label}}
+    </a>
+    @endforeach
+</div>
+
 <ul>
     @forelse ($books as $book)
     <li class="mb-4">
@@ -19,7 +41,7 @@
                 </div>
                 <div>
                     <div class="book-rating">
-                        {{number_format($book->reviews_avg_rating, 1)}}
+                        <x-star-rating :rating="$book->reviews_avg_rating" />
                     </div>
                     <div class="book-review-count">
                         out of {{$book->reviews_count}} {{Str::plural('review', $book->reviews_count)}}
